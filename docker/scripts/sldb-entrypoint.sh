fae7240a8348bbaa117942837abecc651ff4a155
Amadeus Folego <amadeusfolego@gmail.com>
2022-02-01 06:02:36 -0300

    Add more config stuff

--- docker/scripts/sldb-entrypoint.sh
+++ docker/scripts/sldb-entrypoint.sh
@@ -22,6 +22,14 @@ do
   perl updateConfFile.pl etc/$conf_file.conf $conf_settings
 done
 
+touch /etc/sldb/sldb.conf
+mv /etc/sldb/sldb.conf etc/sldb.conf
+perl updateConfFile.pl etc/sldb.conf $SLDB_CONF
+
+[[ ! -z ${LEVELS_CONF+x} ]] && rm -f etc/levels.conf && printf "${LEVELS_CONF}" | sed -e 's/^\s\+//g' > etc/levels.conf
+[[ ! -z ${USERS_CONF+x} ]] && rm -f etc/levels.conf && printf "${USERS_CONF}" | sed -e 's/^\s\+//g' > etc/users.conf
+[[ ! -z ${COMMANDS_CONF+x} ]] && rm -f etc/levels.conf && printf "${COMMANDS_CONF}" | sed -e 's/^\s\+//g' > etc/commands.conf
+
 echo "Running $@ at $(printf '%s %s\n' "$(date)")" >> $logfile_var
 
-exec $@ 2>&1 | tee -a /sldb/log/$LOGFILE.log
+exec $@ 2>&1 | tee -a $logfile_var
